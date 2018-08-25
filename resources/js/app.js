19aa0d42300d37498874abcdcd822b5d33b8e490
Anand Kumar <anand@anandkumar.net>
2018-08-25 15:29:00 +0530

    Initial uploads from mythic

---
+++ resources/js/app.js
@@ -0,0 +1,25 @@
+/**
+ * Primary front-end script.
+ *
+ * Primary JavaScript file. Any includes or anything imported should
+ * be filtered through this file and eventually saved back into the
+ * `/dist/js/app.js` file.
+ *
+ * @package   Mythic
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2018, Justin Tadlock
+ * @link      https://themehybrid.com/themes/mythic
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/**
+ * A simple immediately-invoked function expression to kick-start
+ * things and encapsulate our code.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+(function() {
+
+})();
---
+++ resources/js/customize-preview.js
@@ -0,0 +1,15 @@
+/**
+ * Customize preview script.
+ *
+ * This file handles the JavaScript for the live preview frame in the customizer.
+ * Any includes or imports should be handled in this file. The final result gets
+ * saved back into `dist/js/customize-preview.js`.
+ *
+ * @package   Mythic
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2018, Justin Tadlock
+ * @link      https://themehybrid.com/themes/mythic
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+import { customHeader } from './customize-preview/custom-header';
